7e13ea50a606847b870d91d5f6d8d9abf36f1746
Victor Krogh Hansen <kingodk@outlook.dk>
2023-02-13 17:20:18 +0100

    Added Log class

--- Atom/Source/Core/Base.cpp
+++ Atom/Source/Core/Base.cpp
@@ -1,14 +1,22 @@
 #include "Base.h"
 
+#include "Log.h"
+
 namespace Atom
 {
 
 	void InitializeEngine()
 	{
+		Log::Initialize();
+
+		AT_CORE_TRACE("Atom initializing");
 	}
 
 	void ShutdownEngine()
 	{
+		AT_CORE_TRACE("Atom shutting down");
+
+		Log::Shutdown();
 	}
 
 }
