c87e6124869d404508943dbbc2fa0ae5c691eabc
Victor Krogh Hansen <kingodk@outlook.dk>
2023-02-17 15:17:59 +0100

    Struct Layout

--- Atom.Core/Source/Vector3.cs
+++ Atom.Core/Source/Vector3.cs
@@ -1,8 +1,11 @@
-﻿namespace Atom
+﻿using System.Runtime.InteropServices;
+
+namespace Atom
 {
     // C# 
     // struct (value type) -> stack allocated
     // class (ref type) -> heap allocated (+ GC)
+    [StructLayout(LayoutKind.Sequential)]
     public struct Vector3
     {
         public Vector3(float x, float y, float z)
