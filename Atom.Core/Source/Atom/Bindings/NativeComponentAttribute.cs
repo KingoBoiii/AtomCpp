fdca20ec6836a0c9841c8fd93947f6187436b996
Victor Krogh Hansen <kingodk@outlook.dk>
2023-02-28 01:53:18 +0100

    Added Native Component attribute

---
+++ Atom.Core/Source/Atom/Bindings/NativeComponentAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atom.Bindings
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    internal class NativeComponentAttribute : Attribute
+    {
+        public string NativeClassName { get; set; }
+
+        public NativeComponentAttribute(string nativeClassName)
+        {
+            NativeClassName = nativeClassName;
+        }
+    }
+
+}
