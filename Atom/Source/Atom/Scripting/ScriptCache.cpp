722ed832ae8c158ffbccc3d5fe120acfab12e1b4
Victor Krogh Hansen <kingodk@outlook.dk>
2023-02-28 19:57:22 +0100

    Moved Hash into Core

--- Atom/Source/Atom/Scripting/ScriptCache.cpp
+++ Atom/Source/Atom/Scripting/ScriptCache.cpp
@@ -9,30 +9,6 @@
 namespace Atom
 {
 
-	class Hash
-	{
-	public:
-		static constexpr uint32_t GenerateFNVHash(const char* str)
-		{
-			constexpr uint32_t FNV_PRIME = 16777619u;
-			constexpr uint32_t OFFSET_BASIS = 2166136261u;
-
-			const size_t length = std::string_view(str).length() + 1;
-			uint32_t hash = OFFSET_BASIS;
-			for(size_t i = 0; i < length; ++i)
-			{
-				hash ^= *str++;
-				hash *= FNV_PRIME;
-			}
-			return hash;
-		}
-
-		static constexpr uint32_t GenerateFNVHash(std::string_view string)
-		{
-			return GenerateFNVHash(string.data());
-		}
-	};
-	
 	struct Cache
 	{
 		std::unordered_map<uint32_t, ManagedClass> Classes;
