9391aea374b625c5eef905b5a914ef2f22a697a0
Victor Hansen <victor.h@skatepro.com>
2023-03-03 22:40:20 +0100

    Added Examples folder and created CatchGame as an example

---
+++ Atomic/SandboxProject/Assets/Scripts/Examples/CatchGame/Player.cs
@@ -0,0 +1,35 @@
+﻿using Atom;
+
+namespace Sandbox.Examples.CatchGame
+{
+
+    internal class Player : Entity
+    {
+        [VisibleInEditor] private float Speed = 500.0f;
+
+        private Rigidbody2D m_Rigidbody;
+
+        protected override void Start()
+        {
+            m_Rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        protected override void Update(float deltaTime)
+        {
+            var velocity = new Vector2(0.0f, 0.0f);
+
+            if (Input.IsKeyDown(KeyCode.A))
+            {
+                velocity.X -= Speed * deltaTime;
+            }
+            else if (Input.IsKeyDown(KeyCode.D))
+            {
+                velocity.X += Speed * deltaTime;
+            }
+
+            Log.Info("Velocity: {0}", velocity);
+            m_Rigidbody.SetLinearVelocity(velocity);
+        }
+    }
+
+}
