6d78781ec013510a1d22541f72f379d96f32a213
Victor Krogh Hansen <kingodk@outlook.dk>
2023-03-13 11:09:16 +0100

    Added more renderer api - implementations will be added later!

--- Atom/Source/Atom/Renderer/RendererAPI.h
+++ Atom/Source/Atom/Renderer/RendererAPI.h
@@ -7,8 +7,18 @@ namespace Atom
 	enum class RendererAPI
 	{
 		Unknown = BIT(0),
-		OpenGL = BIT(2),
-		DirectX11 = BIT(1)
+		// Cross-platform
+		OpenGL = BIT(1),
+		// Cross-platform
+		Vulkan = BIT(2),
+
+		// Windows-only
+		DirectX11 = BIT(3),
+		// Windows-only
+		DirectX12 = BIT(4),
+
+		// MacOS-only
+		Metal = BIT(5)
 	};
 
 }
